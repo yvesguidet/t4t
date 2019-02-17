f20594c69797a4a7f45b1d34a97f433cc061fa81
Yves Guidet <yves.guidet@gmail.com>
2019-02-17 16:18:41 +0100

    script de test 	nouveau fichier : valid.sh

---
+++ valid.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+#	l = [-4]
+#	#	l = []
+#	x = temps(l)
+#	print('__main__ : x = {}, l = {}'.format(x, l))
+##
+#	l = [-4, 4]
+#	x = temps(l)
+#	print('__main__ : x = {}, l = {}'.format(x, l))
+##
+#	l = [-5, 5, 4, 32, -33, -0.1]
+#	x = temps(l)
+#	print('__main__ : x = {}, l = {}'.format(x, l))
+##
+#	l = [0.2, 0.1, -5, 5, 4, 32, -33, -0.1]
+#	x = temps(l)
+#	print('__main__ : x = {}, l = {}'.format(x, l))
+#
+#	l = [0.2, 0.1, -5, 0, 5, 4, 32, -33, -0.1]
+#	x = temps(l)
+#	print('__main__ : x = {}, l = {}'.format(x, l))
