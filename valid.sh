4b07b0b5f80aef8ea4de86c9e4c898fd47583104
Yves Guidet <yves.guidet@gmail.com>
2019-02-19 06:57:16 +0100

    test 4 	modifié :         valid.sh

--- valid.sh
+++ valid.sh
@@ -23,17 +23,27 @@
 echo test 1
 echo 1 > temp.txt
 echo -4 >> temp.txt
-python3 noFunc.py < temp.txt
+python3 t4t.py < temp.txt
 
 echo test 2
 echo 2 > temp.txt
 echo -4 >> temp.txt
 echo 4 >> temp.txt
-python3 noFunc.py < temp.txt
+python3 t4t.py < temp.txt
 
 echo test 3
 echo 2 > temp.txt
 echo 4 >> temp.txt
 echo -4 >> temp.txt
-python3 noFunc.py < temp.txt
+python3 t4t.py < temp.txt
+
+#	l = [-5, 5, 4, 32, -33, -0.1]
+echo test 4
+echo 6 > temp.txt
+echo -5 >> temp.txt
+echo 5 >> temp.txt
+echo 32 >> temp.txt
+echo -33 >> temp.txt
+echo -0.1 >> temp.txt
+python3 t4t.py < temp.txt
 
