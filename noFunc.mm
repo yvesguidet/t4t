e6f570a0dfeed2d0f61819403ed2349f37a4754b
Yves Guidet <yves.guidet@gmail.com>
2019-02-19 20:27:25 +0100

    supprimé :        noFunc.mm

--- noFunc.mm
+++
@@ -1,7 +0,0 @@
-<map version="1.0.1">
-	<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-	<!-- ligne bidon -->
-	<node TEXT="noFunc">
-<node TEXT="dimanche 17 f&#233;vrier 2019, 16:03:11 (UTC+0100)"/>
-	<node TEXT="dim*"/><node TEXT="dir" LINK="/home/yves/2011/dev/Python/outils/giclee/aBovo/ghGl/Temperatures4Thom/noFunc"/></node>
-	</map>
